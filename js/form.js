957323da1cf8f8390b98ff27904e4dd86d0d30a0
Carlos Pena <carlos.cpa26@gmail.com>
2023-10-08 13:06:44 -0600

    Selecting labels from HTML and creating function

---
+++ js/form.js
@@ -0,0 +1,11 @@
+let nombre = document.getElementById('nombre').value;
+let fechaNacimiento = document.getElementById('fechaNacimiento').value;
+let direccion = document.getElementById('direccion').value;
+let tipoDocumento = document.getElementById('tipoDocumento').value;
+let numeroDocumento = document.getElementById('numeroDocumento').value;
+let telefono = document.getElementById('telefono').value;
+let motivoConsulta = document.getElementById('motivoConsulta').value;
+
+function GuardarPaciente() {
+
+}
