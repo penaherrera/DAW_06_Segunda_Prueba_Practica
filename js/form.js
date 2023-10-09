2fc7c153e39e92696e85f1a1c0b495491116bf33
penaherrera <cpena@ioogo.com>
2023-10-09 09:41:34 -0600

    Adding guardarPaciente function logic

--- js/form.js
+++ js/form.js
@@ -6,6 +6,33 @@ let numeroDocumento = document.getElementById('numeroDocumento').value;
 let telefono = document.getElementById('telefono').value;
 let motivoConsulta = document.getElementById('motivoConsulta').value;
 
-function GuardarPaciente() {
-
-}
+let paciente = {
+  nombre: nombre,
+  fechaNacimiento: fechaNacimiento,
+  direccion: direccion,
+  tipoDocumento: tipoDocumento,
+  numeroDocumento: numeroDocumento,
+  telefono: telefono,
+  motivoConsulta: motivoConsulta
+};
+function guardarPaciente() {
+      
+    let pacientesGuardados = JSON.parse(localStorage.getItem('pacientes')) || [];
+  
+    pacientesGuardados.push(paciente);
+  
+    localStorage.setItem('pacientes', JSON.stringify(pacientesGuardados));
+  
+    document.getElementById('nombre').value = '';
+    document.getElementById('fechaNacimiento').value = '';
+    document.getElementById('direccion').value = '';
+    document.getElementById('tipoDocumento').value = '';
+    document.getElementById('numeroDocumento').value = '';
+    document.getElementById('telefono').value = '';
+    document.getElementById('motivoConsulta').value = '';
+  
+    alert('La información del paciente ha sido registrada correctamente.');
+  }
+  
+  document.getElementById('guardar').addEventListener('click', guardarPaciente);
+  
