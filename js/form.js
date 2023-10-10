df7740b63f38896757fd6b175dcacf5cf15b02fa
Carlos Pena <carlos.cpa26@gmail.com>
2023-10-09 21:47:52 -0600

    Capturing vars inside function, js ref added to html

--- js/form.js
+++ js/form.js
@@ -1,28 +1,28 @@
-let nombre = document.getElementById('nombre').value;
-let fechaNacimiento = document.getElementById('fechaNacimiento').value;
-let direccion = document.getElementById('direccion').value;
-let tipoDocumento = document.getElementById('tipoDocumento').value;
-let numeroDocumento = document.getElementById('numeroDocumento').value;
-let telefono = document.getElementById('telefono').value;
-let motivoConsulta = document.getElementById('motivoConsulta').value;
-
-let paciente = {
-  nombre: nombre,
-  fechaNacimiento: fechaNacimiento,
-  direccion: direccion,
-  tipoDocumento: tipoDocumento,
-  numeroDocumento: numeroDocumento,
-  telefono: telefono,
-  motivoConsulta: motivoConsulta
-};
 function guardarPaciente() {
-      
+    let nombre = document.getElementById('nombre').value;
+    let fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    let direccion = document.getElementById('direccion').value;
+    let tipoDocumento = document.getElementById('tipoDocumento').value;
+    let numeroDocumento = document.getElementById('numeroDocumento').value;
+    let telefono = document.getElementById('telefono').value;
+    let motivoConsulta = document.getElementById('motivoConsulta').value;
+
+    let paciente = {
+        nombre: nombre,
+        fechaNacimiento: fechaNacimiento,
+        direccion: direccion,
+        tipoDocumento: tipoDocumento,
+        numeroDocumento: numeroDocumento,
+        telefono: telefono,
+        motivoConsulta: motivoConsulta
+    };
+
     let pacientesGuardados = JSON.parse(localStorage.getItem('pacientes')) || [];
-  
+
     pacientesGuardados.push(paciente);
-  
+
     localStorage.setItem('pacientes', JSON.stringify(pacientesGuardados));
-  
+
     document.getElementById('nombre').value = '';
     document.getElementById('fechaNacimiento').value = '';
     document.getElementById('direccion').value = '';
@@ -30,9 +30,10 @@ function guardarPaciente() {
     document.getElementById('numeroDocumento').value = '';
     document.getElementById('telefono').value = '';
     document.getElementById('motivoConsulta').value = '';
-  
+
     alert('La información del paciente ha sido registrada correctamente.');
-  }
-  
-  document.getElementById('guardar').addEventListener('click', guardarPaciente);
-  
+
+    console.log(pacientesGuardados);
+}
+
+document.getElementById('guardar').addEventListener('click', guardarPaciente);
